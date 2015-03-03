205de8bd7c5e3255dec24105d3cb6b5f89ff6520
Sean Kim <skim2636@gmail.com>
2015-03-02 18:09:59 -0800

    user directed sign in page and able to post username and hashed password

---
+++ app/models/user.js
@@ -0,0 +1,19 @@
+var db = require('../config');
+var bcrypt = require('bcrypt-nodejs');
+var Promise = require('bluebird');
+
+var User = db.Model.extend({
+  tableName: 'users',
+  hasTimestamps: true,
+
+  initialize: function(){
+    var context = this;
+    bcrypt.genSalt(10, function (err, salt) {
+      bcrypt.hash(context.get('password'), salt, null, function (err, hash) {
+        context.set('password', hash);
+      });
+    });
+  }
+});
+
+module.exports = User;
